914051a059753d2efd12b9ab2ae0bd7ab216a54e
hungit1993qt <hungit1993qt@gmail.com>
2022-11-30 15:48:03 +0700

    fix

--- src/Components/Header/TopMenu.tsx
+++ src/Components/Header/TopMenu.tsx
@@ -125,7 +125,7 @@ const TopMenu = (props: Props) => {
                   );
                 })}
               </tbody>
-              <tbody>
+              {/* <tbody>
                 <tr>
                   <th className="sticky bottom-0 border-0 px-6 py-3 bg-white">
                     <div className="flex justify-center font-semibold">
@@ -133,7 +133,7 @@ const TopMenu = (props: Props) => {
                     </div>
                   </th>
                 </tr>
-              </tbody>
+              </tbody> */}
             </table>
           </div>
         </div>
