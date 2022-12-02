4579d0ba7d61e4d6ce6e53a056e2fa0723e90d60
hungit1993qt <hungit1993qt@gmail.com>
2022-12-02 19:24:38 +0700

    fix

--- src/Components/Dashboard/Option.tsx
+++ src/Components/Dashboard/Option.tsx
@@ -25,7 +25,7 @@ const Option = (props: Props) => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-2 z-10 top-4 bg-white dark:text-white dark:bg-drop-modal-dark dark:border-boder-drop-modal-dark border-descript-light rounded-md shadow-lg w-36 ">
+          <Menu.Items className="border-transparent absolute right-2 z-10 top-4 bg-white dark:text-white dark:bg-drop-modal-dark dark:border-boder-drop-modal-dark border-descript-light rounded-md shadow-lg w-36 ">
             <div className="py-0 border-0">
               <Menu.Item>
                 {({ active }: any) => (
