b22c66aac2412426e316bfacf217765c787663c4
hungit1993qt <hungit1993qt@gmail.com>
2022-12-03 10:33:15 +0700

    fix

--- src/Components/Dashboard/Calendar.tsx
+++ src/Components/Dashboard/Calendar.tsx
@@ -45,16 +45,16 @@ const meetings = [
     name: "Nguyễn Hoàng Nam",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    startDatetime: "2022-12-02T08:00",
-    endDatetime: "2022-12-02T10:00",
+    startDatetime: "2022-12-03T08:00",
+    endDatetime: "2022-12-03T10:00",
   },
   {
     id: 5,
     name: "Văn Như Ái",
     imageUrl:
       "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    startDatetime: "2022-12-02T10:45",
-    endDatetime: "2022-12-02T12:45",
+    startDatetime: "2022-12-03T10:45",
+    endDatetime: "2022-12-03T12:45",
   },
 ];
 
--- src/Components/Dashboard/WidgetSystems.tsx
+++ src/Components/Dashboard/WidgetSystems.tsx
@@ -8,21 +8,25 @@ const WidgetSystems = (props: Props) => {
       title: "Dự án",
       value: "168",
       icon: "fa fa-briefcase",
+      color:"text-blue-500"
     },
     {
       title: "Tài khoản",
       value: "368",
       icon: "fa fa-user-friends",
+      color:"text-yellow-500"
     },
     {
       title: "Doanh thu",
       value: "868",
       icon: "fa fa-money-bill-wave-alt",
+      color:"text-green-500"
     },
     {
       title: "Lợi nhuận",
       value: "468",
-      icon: "fab fa-sketch",
+      icon: "fa fa-coins",
+      color:"text-fuchsia-500"
     },
   ];
   return (
@@ -36,8 +40,8 @@ const WidgetSystems = (props: Props) => {
         <div className="z-0 mt-4 w-full flex flex-row outline-none flex-wrap xl:flex-wrap">
           {data.map((data,index)=>{
             return(<div key={index} className="flex flex-col xl:w-1/4 lg:w-1/2  w-full gap-y-2  items-center border p-4 shadow-sm">
-              <i className={`${data.icon} text-descript-light text-3xl`}></i>
-              <span className=" font-bold text-2xl">{data.value}</span>
+              <i className={`${data.icon} ${data.color} text-descript-light text-3xl`}></i>
+              <span className=" font-bold text-2xl dark:text-white">{data.value}</span>
               <span className="text-xl text-descript-light">{data.title}</span>
             </div>)
           })}
