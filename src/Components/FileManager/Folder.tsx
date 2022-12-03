04af72d2b4aa587d44b0d5841830e1ef0b5dc1b7
hungit1993qt <hungit1993qt@gmail.com>
2022-12-03 14:05:07 +0700

    fix

--- src/Components/FileManager/Folder.tsx
+++ src/Components/FileManager/Folder.tsx
@@ -44,7 +44,7 @@ const Folder = (props: Props) => {
     <div className="xl:w-72 w-full py-2 pl-2 pr-1 ">
       <div className="shadow box-border border px-4 py-2 bg-box dark:bg-dark h-full flex flex-col items-center">
         <div className="flex justify-center w-full ">
-          <span className="w-4/5 capitalize whitespace-nowrap font-medium rounded-sm shadow-md text-white bg-purple py-2 text-center text-xs mt-2">
+          <span className="w-4/5 capitalize whitespace-nowrap font-medium cursor-pointer rounded-sm shadow-md text-white bg-purple py-2 text-center text-xs mt-2">
             <i className="fa fa-plus mr-1"></i>Tạo mới folder
           </span>
           {/* <i className="fa fa-ellipsis-v cursor-pointer dark:text-white"></i> */}
@@ -53,7 +53,7 @@ const Folder = (props: Props) => {
           <ul>
             {data.map((data, index) => {
               return (
-                <li className="text-base font-normal mb-6 whitespace-nowrap">
+                <li className="text-base font-normal mb-6 cursor-pointer whitespace-nowrap">
                   <i className={`${data.icon} mr-4`}></i>
                   {data.title}
                 </li>
